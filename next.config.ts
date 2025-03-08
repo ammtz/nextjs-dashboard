9c76521c5ddbb7a572f1a1e1f4b4d09a7dac9b01
ammtz <104395529+ammtz@users.noreply.github.com>
2025-03-08 10:37:16 -0500

    Add invoice page...partial

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next';
-
+ 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: '**', // Replace with your actual image domain(s)
+          },
+        ],
+      },
 };
-
+ 
 export default nextConfig;
